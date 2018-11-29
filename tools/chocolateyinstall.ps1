89f5fda59b72a9871348ac5d21843c3757cec946
higan <higan@live.cn>
2018-11-29 21:54:55 +0800

    Add source

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+$jarFile = "$env:ChocolateyPackageFolder\antlr-4.7.1-complete.jar"
+$cmdFile = "$env:ChocolateyPackageFolder\tools\java.cmd"
+$checksum      = 'F41DCE7441D523BAF9769CB7756A00F27A4B67E55AACAB44525541F62D7F6688'
+$checksumType  = 'sha256'
+$url = "https://www.antlr.org/download/antlr-4.7.1-complete.jar"
+
+Get-ChocolateyWebFile -PackageName $env:ChocolateyPackageName -FileFullPath $jarFile -Url $url -Checksum $checksum -ChecksumType  $checksumType
+Install-BinFile -Name $env:ChocolateyPackageName -Path "$cmdFile" -Command $jarFile
